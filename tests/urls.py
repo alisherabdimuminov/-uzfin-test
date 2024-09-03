e0542a538e68ace4c6a85b9aff0e1f04f1682d56
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-09-03 18:40:22 +0500

    nmadur

--- tests/urls.py
+++ tests/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .views import home, test, check_test, create_question
+from users.views import create_user
 
 
 urlpatterns = [
     path("", home, name="home"),
     path("test/<int:pk>/", test, name="test"),
     path("check_test/<int:pk>/", check_test, name="check"),
-    path("create/", create_question, name="create_question"),
+    path("create/question/", create_question, name="create_question"),
+    path("create/user/", create_user, name="create_user"),
 ]
