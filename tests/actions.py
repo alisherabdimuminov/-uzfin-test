e3ad3161c372c829e4097dd5f1c8f160f6519d9c
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-09-04 14:09:37 +0500

    some

--- tests/actions.py
+++ tests/actions.py
@@ -7,18 +7,17 @@ from django.http import HttpRequest, HttpResponse
 
 @admin.action(description="Print selected tests")
 def print_selected_tests(modeladmin: ModelAdmin, request: HttpRequest, queryset: QuerySet):
-    th = tuple(["Talabgarnining F.I", "Mutaxasislik", "To'plagan bali", "To'g'ri javoblar", "Texnik nosozlik sababli", ])
-    print(queryset)
+    th = tuple(["Talabgarnining F.I", "Mutaxasislik", "To'plagan bali", "To'g'ri javoblar", "Texnik nosozlik sababli qo'shilgan ball", ])
     td = []
     for test in queryset:
         print(test)
         td += [(f"{test.user.first_name} {test.user.last_name}", f"{test.spec}", f"{test.percentage}", f"{test.correct}", "", )]
     TABLE = [th] + td
 
-    pdf = FPDF()
+    pdf = FPDF(orientation="landscape")
     pdf.add_page()
     pdf.set_font("Times", size=16)
-    with pdf.table() as table:
+    with pdf.table(col_widths=(30, 30, 10, 10, 20)) as table:
         for data_row in TABLE:
             row = table.row()
             for datum in data_row:
