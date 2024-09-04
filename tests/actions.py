637b49d16fb275fe612254da0b3a637f7b589f76
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-09-04 14:11:37 +0500

    some

--- tests/actions.py
+++ tests/actions.py
@@ -7,17 +7,18 @@ from django.http import HttpRequest, HttpResponse
 
 @admin.action(description="Print selected tests")
 def print_selected_tests(modeladmin: ModelAdmin, request: HttpRequest, queryset: QuerySet):
-    th = tuple(["Talabgarnining F.I", "Mutaxasislik", "To'plagan bali", "To'g'ri javoblar", "Texnik nosozlik sababli qo'shilgan ball", ])
+    th = tuple(["No", "Talabgarnining F.I", "Mutaxasislik", "To'plagan bali", "To'g'ri javoblar", "Texnik nosozlik sababli qo'shilgan ball", ])
     td = []
+    i = 1
     for test in queryset:
-        print(test)
-        td += [(f"{test.user.first_name} {test.user.last_name}", f"{test.spec}", f"{test.percentage}", f"{test.correct}", "", )]
+        td += [(f"{i}", f"{test.user.first_name} {test.user.last_name}", f"{test.spec}", f"{test.percentage}", f"{test.correct}", "", )]
+        i += 1
     TABLE = [th] + td
 
     pdf = FPDF(orientation="landscape")
     pdf.add_page()
     pdf.set_font("Times", size=16)
-    with pdf.table(col_widths=(30, 30, 10, 10, 20)) as table:
+    with pdf.table(col_widths=(5, 25, 30, 10, 10, 20)) as table:
         for data_row in TABLE:
             row = table.row()
             for datum in data_row:
